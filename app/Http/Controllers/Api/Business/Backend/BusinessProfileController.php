03ab06b1c571411b2ddcc768f0fdf47ec2284373
unknown <alauddinnajim202@gmail.com>
2025-01-11 05:43:07 -0800

    Remove required validation for 'is_closed' field in business hours and update its assignment logic in BusinessProfileController

--- app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
+++ app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
@@ -37,7 +37,7 @@ class BusinessProfileController extends Controller
             'hours' => 'required|array',
             'hours.*.day' => 'required|string',
             // 'hours.*.date' => 'required',
-            'hours.*.is_closed' => 'required',
+            // 'hours.*.is_closed' => 'required',
             'hours.*.open_time' => 'nullable|string',
             'hours.*.close_time' => 'nullable|string',
 
@@ -78,7 +78,7 @@ class BusinessProfileController extends Controller
             $businessProfile->business_hours()->create([
                 'day' => $hour['day'],
                 // 'date' => $hour['date'],
-                'is_closed' => $hour['is_closed'],
+                'is_closed' => $request->is_closed,
                 'open_time' => $hour['is_closed'] ? null : $hour['open_time'],
                 'close_time' => $hour['is_closed'] ? null : $hour['close_time'],
             ]);
