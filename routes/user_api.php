bb194fd59eed96553a0f23f64f8393763e03f067
unknown <alauddinnajim202@gmail.com>
2025-01-28 15:40:59 +0600

    Update event ticket route for consistency in API structure

--- routes/user_api.php
+++ routes/user_api.php
@@ -72,7 +72,7 @@ Route::middleware(['auth:user', 'role:user'])->prefix('auth-user')->group(functi
     Route::post('event/{id}/booking', [EventBookingController::class, 'event_book']);
     Route::get('event-booking/{id}/order-summary', [EventBookingController::class, 'order_summary']);
     // event ticket
-    Route::get('event/{id}/ticket', [EventBookingController::class, 'event_ticket']);
+    Route::get('event-booking/{id}/ticket', [EventBookingController::class, 'event_ticket']);
     Route::get('event/{id}/ticket-download', [EventBookingController::class, 'download_ticket'])->name('user.event_ticket_download');
 
   
