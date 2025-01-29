62ce82e95ef5165f04611cff24658557ddde63c5
unknown <alauddinnajim202@gmail.com>
2025-01-29 17:52:39 +0600

    Update validation rule for is_second_time field to remove boolean requirement

--- app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
+++ app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
@@ -41,7 +41,7 @@ class BusinessProfileController extends Controller
             'hours.*.open_time' => 'nullable|string',
             'hours.*.close_time' => 'nullable|string',
 
-            'hours.*.is_second_time' => 'required|boolean',
+            'hours.*.is_second_time' => 'required',
 
             'hours.*.re_open_time' => 'nullable|string',
             'hours.*.re_close_time' => 'nullable|string',
