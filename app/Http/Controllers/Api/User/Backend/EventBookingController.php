e3e2ec31eb50e5b052c2618c32e8fec917eec774
unknown <alauddinnajim202@gmail.com>
2025-01-29 12:15:24 +0600

    Add error handling for missing event booking in download_ticket method

--- app/Http/Controllers/Api/User/Backend/EventBookingController.php
+++ app/Http/Controllers/Api/User/Backend/EventBookingController.php
@@ -206,6 +206,10 @@ class EventBookingController extends Controller
     {
         $booking = EventBooking::with('guests', 'business_profile')->findOrFail($id);
 
+        // if event booking not found
+        if (!$booking) {
+            return $this->error([], 'Event booking not found.', 404);
+        }
         $data = [
 
             'user_id' => $booking->user_id,
