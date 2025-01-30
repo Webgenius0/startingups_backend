49873f828d0969e898d802826ae84aa2f8c299d5
unknown <alauddinnajim202@gmail.com>
2025-01-30 14:36:14 +0600

    Remove maximum file size restriction for cover image in business profile update

--- app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
+++ app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
@@ -192,7 +192,7 @@ class BusinessProfileController extends Controller
         }
 
         $validatedData = $request->validate([
-            'cover' => 'nullable|image|mimes:jpg,jpeg,png|max:4096',
+            'cover' => 'nullable|image|mimes:jpg,jpeg,png',
             'business_name' => 'required|string',
             'category_id' => 'required|integer',
             'sub_category_id' => 'required|integer',
