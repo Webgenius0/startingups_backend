947e6dc36fc2bf802c8c8c89938d46dfbdfa3f82
unknown <alauddinnajim202@gmail.com>
2025-01-31 11:19:05 +0600

    Fix business hours creation logic to correctly handle 'is_closed' and ensure proper time values are set

--- app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
+++ app/Http/Controllers/Api/Business/Backend/BusinessProfileController.php
@@ -233,13 +233,13 @@ class BusinessProfileController extends Controller
                     foreach ($hours as $hour) {
                         $businessProfile->business_hours()->create([
                             'day' => $hour['day'],
-                            'is_closed' => $hour['is_closed'] == true ? 1 : 0,
-                            'open_time' => $hour['is_closed'] ? null : $hour['open_time'],
-                            'close_time' => $hour['is_closed'] ? null : $hour['close_time'],
+                            'is_closed' => $hour['is_closed'] == true ? 0 : 1,
+                            'open_time' =>  $hour['open_time'],
+                            'close_time' =>  $hour['close_time'],
 
                             'is_second_time' => $hour['is_second_time'] == true ? 1 : 0,
-                            're_open_time' => isset($hour['re_open_time']) && !$hour['is_closed'] ? $hour['re_open_time'] : null,
-                            're_close_time' => isset($hour['re_close_time']) && !$hour['is_closed'] ? $hour['re_close_time'] : null,
+                            're_open_time' =>  $hour['re_open_time'] ,
+                            're_close_time' =>  $hour['re_close_time'] ,
                         ]);
                     }
                 }
