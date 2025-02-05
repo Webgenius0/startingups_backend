b202bd88768a98b9ff5fa7ed841500af69a71ec7
unknown <alauddinnajim202@gmail.com>
2025-02-05 13:04:19 +0600

    Enhance event retrieval in UserHomeController to include business hours in upcoming events query

--- app/Http/Controllers/Api/User/Backend/UserHomeController.php
+++ app/Http/Controllers/Api/User/Backend/UserHomeController.php
@@ -285,7 +285,7 @@ class UserHomeController extends Controller
     public function events()
     {
 
-        $upcoming_events = BusinessProfile::with('business_hours')->orderBy('date', 'asc')
+        $upcoming_events = BusinessProfile::with('business_hours')->orderBy('created_at', 'asc')
             ->get();
 
         // if ($upcoming_events->isEmpty()) {
