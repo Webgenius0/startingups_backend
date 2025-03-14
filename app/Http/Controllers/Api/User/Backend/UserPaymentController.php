289ce53124cc3c143fab8230c42efd97f1ed4260
unknown <alauddinnajim202@gmail.com>
2025-03-14 10:13:04 +0600

    Enable error handling for Stripe account onboarding and transfer capabilities in UserPaymentController

--- app/Http/Controllers/Api/User/Backend/UserPaymentController.php
+++ app/Http/Controllers/Api/User/Backend/UserPaymentController.php
@@ -42,15 +42,15 @@ class UserPaymentController extends Controller
         $eventBooking = EventBooking::with('business_profile')->findOrFail($request->event_booking_id);
         $event_owner = $eventBooking->business_profile->user;
 
-        // if (!$event_owner->stripe_account_id) {
-        //     return $this->error([], 'Event owner is not onboarded to Stripe.', 400);
-        // }
+        if (!$event_owner->stripe_account_id) {
+            return $this->error([], 'Event owner is not onboarded to Stripe.', 400);
+        }
 
         $account = Account::retrieve($event_owner->stripe_account_id);
 
-        // if (!$account->capabilities->transfers || $account->capabilities->transfers !== 'active') {
-        //     return $this->error([], 'The event owner\'s account is not enabled for transfers.', 400);
-        // }
+        if (!$account->capabilities->transfers || $account->capabilities->transfers !== 'active') {
+            return $this->error([], 'The event owner\'s account is not enabled for transfers.', 400);
+        }
 
         try {
 
