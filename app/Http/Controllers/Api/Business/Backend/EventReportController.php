1391a76395f5b93bb1b4d1eda90a021271a66ce3
unknown <alauddinnajim202@gmail.com>
2025-03-17 15:41:16 +0600

    Refactor event ratings calculation to ensure percentage is calculated correctly and formatted as a string

--- app/Http/Controllers/Api/Business/Backend/EventReportController.php
+++ app/Http/Controllers/Api/Business/Backend/EventReportController.php
@@ -110,11 +110,11 @@ class EventReportController extends Controller
         $ratings = ['a' => 5, 'b' => 4, 'c' => 3, 'd' => 2, 'e' => 1];
 
         foreach ($ratings as $key => $value) {
-            // if ($reviewCount > 0) {
-            //     $percentage = round(($ratingCounts->get($value, 0) / $reviewCount) * 100, 2);
-            // } else {
+            if ($reviewCount > 0) {
+                $percentage = round(($ratingCounts->get($value, 0) / $reviewCount) * 100, 2);
+            } else {
                 $percentage = 0.00; // directly as float
-            // }
+            }
             $ratingPercentages[$key] = $percentage;
         }
 
@@ -138,7 +138,9 @@ class EventReportController extends Controller
             'event_id' => $event->id,
             'average_rating' => $averageRating,
             'total_reviews' => $reviewCount,
-            'rating_percentages' => $ratingPercentages,
+            'rating_percentages' => collect($ratingPercentages)->map(function ($val) {
+                return number_format($val, 2, '.', ''); // formats as string "0.00"
+            }),
             'reviews' => $reviews,
         ], 'Business profile ratings fetched successfully.');
     }
